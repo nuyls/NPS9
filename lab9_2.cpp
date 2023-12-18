b159b8009ff7cc4f3fa54f533699337c5db62e19
NpS <preeyanan.nps@gmail.com>
2023-12-18 15:22:48 +0700

    9.2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include<iomanip>
+#include<string>
 using namespace std;
-
-//Write the function printO() here
+void printO(double n, double m){
+   if(n>0&&m>0){
+   for(int i=0; i<n; i++){
+   for(int a=0; a<m; a++){
+   cout <<"O";
+   }
+   cout << "\n";
+   }}else {
+   cout << "Invalid input";
+   }}
+   
 
 int main(){
 	
